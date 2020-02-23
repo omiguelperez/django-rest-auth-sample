04457853e88bce81f0b3125083735c3e6975ab26
Oscar Pérez <mr.omiguelperez@gmail.com>
2020-02-23 17:17:30 -0500

    Add django rest auth and swagger

--- djrestauthsample/settings.py
+++ djrestauthsample/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'drf_yasg',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_cdn')
